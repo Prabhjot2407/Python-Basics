65de2212776072c8e25e95d38349376c2dccd6ee
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-11-24 11:28:27 +0530

    Add files via upload

---
+++ Lecture17.py
@@ -0,0 +1,35 @@
+# Lambda function
+# Lambda function is a small, anonymous(nameless), functions in python
+# It can take any number of args but can have only one expression
+# lambda args:expression
+
+# sq = lambda x: x**2
+# print(sq(5))
+
+# max = lambda a,b: a if a>b else b
+# print(max(10, 20))
+
+# nums = [1, 2, 3, 4, 5]
+# sq = list(map(lambda x: x**2,nums))
+# print(sq)
+
+# Sum of row elements in jag list
+# jag =[
+#     [1, 2, 3],
+#     [4, 5],
+#     [6]
+# ]
+# for row in jag:
+#     print("Sum of each row is :", sum(row))
+
+# 2-D list
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# print(matrix[1][2])
+# for row in matrix:
+#     for val in row:
+#         print(val, end=" ")
+#     print()   
